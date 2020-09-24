79378d434adc1ebded9695ef80653296d67a09ed
tayDias <tay.mdias67@gmail.com>
2020-09-24 01:34:10 -0300

    Direcionamento a produção + inclusão de bot

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
+//Production
+//const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')
+const rootElement = document.getElementById('root')
+
+ReactDOM.render( 
+  <BrowserRouter basename = "%PUBLIC_URL%/">
+    <App />
+  </BrowserRouter>, rootElement
+)
+
+//Dev
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
+*/
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    //Production
+    baseURL: 'https://proj-vs-api.herokuapp.com/'
+    
+    //Dev
+    //baseURL: 'http://localhost:3030'
 })
 
 export default api
