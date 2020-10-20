46ec827c257d167a1a64f9a0ab3cff328de18359
tayDias <tay.mdias67@gmail.com>
2020-10-19 22:57:33 -0300

    Estilização do menu da pagina profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { } from 'react'
+import React, { useState } from 'react'
 
 import PageHeader from '../../components/PageHeader'
 import Notification from '../../components/Notification'
@@ -7,11 +7,25 @@ import Schedule from '../../components/Schedule'
 import './styles.css'
 
 function Profile() {
-    //function loadUserInformation() {}
-    //function loadSchedules() {}
+    const [menuOption, setMenuOption] = useState("notification")
+    const [agendaClassname, setAgendaClassname] = useState("off")
+    const [notificationClassname, setNotificationClassname] = useState("on")
 
-    function changeButton() {
-        //change button class
+    //function loadUserInformation() {}
+    //function loadSchedules() {}   
+    //function loadNotifications() {}
+
+    function changeMenuOption(id: string) {
+        if(id === "agenda") {
+            setMenuOption("agenda")
+            setAgendaClassname("on")
+            setNotificationClassname("off")
+        }
+        else {
+            setMenuOption("notification")
+            setAgendaClassname("off")
+            setNotificationClassname("on")
+        }
     }
 
     return (
@@ -26,8 +40,8 @@ function Profile() {
                     <button 
                         type="button" 
                         id="notification" 
-                        className="on" 
-                        onClick={changeButton}
+                        className={notificationClassname} 
+                        onClick={() => changeMenuOption("notification")}
                     >
                         Notificações
                     </button>
@@ -36,8 +50,8 @@ function Profile() {
                     <button 
                         type="button" 
                         id="agenda" 
-                        className="off" 
-                        onClick={changeButton}
+                        className={agendaClassname} 
+                        onClick={() => changeMenuOption("agenda")}
                     >
                         Agenda
                     </button>
@@ -45,33 +59,41 @@ function Profile() {
             </form>
 
             <main>
-                {/*<p className="without-notifications">Nenhuma notificação de solicitação de auxílio.</p>*/}
-                <Notification 
-                    title='Diego precisa de ajuda!'
-                    preview='Preview.'
-                />
-                <Notification 
-                    title='Maria precisa de ajuda!'
-                    preview='Teste.'
-                />
-                {/*<Schedule 
-                    from=""
-                    to=""
-                    weekday=""
-                />
-                <Schedule 
-                    from=""
-                    to=""
-                    weekday=""
-                />
-                <div className="agenda-buttons">
-                    <div className="new-schedule">
-                        <button type="button" >Incluir Novo</button>
-                    </div>
-                    <div className="save-schedules">
-                        <button type="button" >Salvar Alterações</button>
-                    </div>
-                </div>*/}
+                {/*<p className="without-notifications">Nenhuma notificação de solicitação de auxílio.</p>   */}
+                { menuOption === "notification" ? 
+                    <div>
+                        <Notification 
+                            title='Diego precisa de ajuda!'
+                            preview='Preview.'
+                        />
+                        <Notification 
+                            title='Maria precisa de ajuda!'
+                            preview='Teste.'
+                        />
+                    </div> 
+                    :
+                    <div>
+                        <Schedule 
+                            from=""
+                            to=""
+                            weekday=""
+                        /> 
+                        <Schedule 
+                            from=""
+                            to=""
+                            weekday=""
+                        />
+                    
+                        <div className="agenda-buttons">
+                            <div className="new-schedule">
+                                <button type="button" >Incluir Novo</button>
+                            </div>
+                            <div className="save-schedules">
+                                <button type="button" >Salvar Alterações</button>
+                            </div>
+                        </div> 
+                    </div> 
+                }
             </main>
         </div>
     )
