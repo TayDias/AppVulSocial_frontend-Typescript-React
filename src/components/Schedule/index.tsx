d77f6d81f26b45fee889ed744deb0c03ce8b0234
tayDias <tay.mdias67@gmail.com>
2020-10-23 00:41:32 -0300

    Update e insert de novos itens da agenda concluidos + ajustes de layout dos itens

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -1,46 +1,61 @@
-import React from 'react'
+import React, { InputHTMLAttributes, useState } from 'react'
 import StyledInput from '../StyledInput'
 import StyledSelect from '../StyledSelect'
 
 import './styles.css'
 
-interface PageScheduleProps {
-    from: string,
-    to: string,
-    weekday: string
+interface PageScheduleProps extends InputHTMLAttributes<HTMLInputElement>  {
+    scheduleItems: {
+        id: number;
+        weekday: string;
+        from: string;
+        to: string;
+        toUpdate: boolean;
+    }[]
+    alterSchedule: any
 }
 
-const Schedule: React.FC<PageScheduleProps> = props => {
+const Schedule: React.FC<PageScheduleProps> = ( { alterSchedule, scheduleItems,  ...rest } )  => {
     return (
         <article className="schedule">
-            <div className="input-block">
-                <div key={Math.random()} className="schedule-item">
-                    <StyledSelect 
-                        name="weekday" 
-                        label="Dia da Semana"
-                            options={[
-                                {id: '0', value: 'Domingo', label: 'Domingo'},
-                                {id: '1', value: 'Segunda-Feira', label: 'Segunda-Feira'},
-                                {id: '2', value: 'Terça-Feira', label: 'Terça-Feira'},
-                                {id: '3', value: 'Quarta-Feira', label: 'Quarta-Feira'},
-                                {id: '4', value: 'Quinta-Feira', label: 'Quinta-Feira'},
-                                {id: '5', value: 'Sexta-Feira', label: 'Sexta-Feira'},
-                                {id: '6', value: 'Sábado', label: 'Sábado'}                     
-                            ]}
-                        />
+            {scheduleItems.map((scheduleItem, index) => {
+                return (
+                    <div key={Math.random()} className="input-block">
+                        <div className="schedule-item">
+                            <StyledSelect 
+                                name="weekday" 
+                                label="Dia da Semana"
+                                value={scheduleItem.weekday}
+                                onChange={e => alterSchedule(index, 'weekday', e.target.value)}
+                                options={[
+                                    {id: '0', value: 'Domingo', label: 'Domingo'},
+                                    {id: '1', value: 'Segunda-Feira', label: 'Segunda-Feira'},
+                                    {id: '2', value: 'Terça-Feira', label: 'Terça-Feira'},
+                                    {id: '3', value: 'Quarta-Feira', label: 'Quarta-Feira'},
+                                    {id: '4', value: 'Quinta-Feira', label: 'Quinta-Feira'},
+                                    {id: '5', value: 'Sexta-Feira', label: 'Sexta-Feira'},
+                                    {id: '6', value: 'Sábado', label: 'Sábado'}                    
+                                ]}             
+                            />
 
-                        <StyledInput 
-                            name="from" 
-                            label="Das" 
-                            type="time"
-                        />
-                        <StyledInput 
-                            name="to" 
-                            label="Até" 
-                            type="time"
-                        />
-                </div>     
-            </div>
+                            <StyledInput 
+                                name="from" 
+                                label="Das" 
+                                type="time"
+                                value={scheduleItem.from}
+                                onChange={e => alterSchedule(index, 'from', e.target.value)}
+                            />
+                            <StyledInput 
+                                name="to" 
+                                label="Até" 
+                                type="time"
+                                value={scheduleItem.to}
+                                onChange={e => alterSchedule(index, 'to', e.target.value)}
+                            />
+                        </div>     
+                    </div>
+                )
+            })} 
         </article>
     )
 }
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,27 +1,115 @@
-import React, { useState } from 'react'
+import React, { FormEvent, useState, useEffect } from 'react'
 
 import PageHeader from '../../components/PageHeader'
-import Notification from '../../components/Notification'
+//import Notification from '../../components/Notification'
 import Schedule from '../../components/Schedule'
 
 import './styles.css'
 
+import api from '../../services/api'
+
 function Profile() {
+    const rescuer_id = localStorage.getItem('rescuer_id')
+
     const [menuOption, setMenuOption] = useState("notification")
     const [agendaClassname, setAgendaClassname] = useState("off")
     const [notificationClassname, setNotificationClassname] = useState("on")
 
-    //function loadUserInformation() {}
-    //function loadSchedules() {}   
-    //function loadNotifications() {}
+    const [scheduleItems, setScheduleItems] = useState([
+        { id: 0, weekday: 'Domingo', from: '', to: '', toUpdate: true }
+    ])
+
+    const [userData, setUserData] = useState({
+        name: "",
+        type: "",
+        email: "",
+        bio: "",
+        available: 1
+    })
+
+    useEffect(() => {
+        loadUserInformation()
+    }, [])
+
+    /*
+    function loadNotifications() {}
+    */
+
+    function handleInsertUpdate(e: FormEvent) {
+        e.preventDefault()
+
+        const scheduleItemsToInsert = scheduleItems.filter(item => !item.toUpdate)
+        const scheduleItemsToUpdate = scheduleItems.filter(item => item.toUpdate)
+
+        //Insert new items
+        api.post('schedules', {
+            rescuer_id,
+            schedules: scheduleItemsToInsert
+        }).catch(() => {
+            alert('Erro na insersão de novos itens!')
+        })
+
+        //Update items
+        api.put('schedules', {
+            rescuer_id,
+            schedules: scheduleItemsToUpdate
+
+        }).catch(() => {
+            alert('Erro na alteração dos itens!')
+        })
+
+        alert('Agenda atualizada com sucesso!')
+    }
+
+    function addNewScheduleItem(){
+        setScheduleItems([
+            ...scheduleItems,
+            { id: 0, weekday: 'Domingo', from: '', to: '', toUpdate: false }
+        ])
+    }
+
+    function setScheduleItemValue(position:number, field: string, value:string){
+        const propsItems = scheduleItems.map((scheduleItem: any, index: number) => {
+            //Item percorrido igual ao item de alteração
+            if(index === position) {
+                return {
+                    ...scheduleItem,
+                    [field]: value  //sobrescreve o item com o novo valor
+                }
+            }
+            
+            return scheduleItem
+        })
+
+        setScheduleItems(propsItems)
+    }
+
+    async function loadSchedules() {
+        const response = await api.get('schedules', {
+            params: {
+                rescuer_id,
+            }
+        })
+
+        setScheduleItems(response.data)
+        console.log(response.data)
+    }
+
+    async function loadUserInformation() {
+        let id = rescuer_id
+        const response = await api.get(`rescuers/${id}`)
+
+        setUserData(response.data)
+    }
 
     function changeMenuOption(id: string) {
         if(id === "agenda") {
+            loadSchedules()
             setMenuOption("agenda")
             setAgendaClassname("on")
-            setNotificationClassname("off")
+            setNotificationClassname("off")         
         }
-        else {
+        else if (id === "notification") {
             setMenuOption("notification")
             setAgendaClassname("off")
             setNotificationClassname("on")
@@ -31,13 +119,13 @@ function Profile() {
     return (
         <div id="page-profile" className="container">
             <PageHeader 
-                title='Olá Taynara, que bom que você está aqui agora!' 
+                title={`Olá ${userData.name}, que bom que você está aqui agora!`}
                 description="Você pode ver aqui quem está precisando de ajuda  e organizar sua agenda."
             />
 
             <form id="menu-items">
                 <div className="input-block">
-                    <button 
+                    <button
                         type="button" 
                         id="notification" 
                         className={notificationClassname} 
@@ -59,9 +147,7 @@ function Profile() {
             </form>
 
             <main>
-                {/*<p className="without-notifications">Nenhuma notificação de solicitação de auxílio.</p>   */}
-                { menuOption === "notification" ? 
-                    <div>
+                {/*<div>
                         <Notification 
                             title='Diego precisa de ajuda!'
                             preview='Preview.'
@@ -70,29 +156,28 @@ function Profile() {
                             title='Maria precisa de ajuda!'
                             preview='Teste.'
                         />
-                    </div> 
+                    </div>
+                */}
+                { menuOption === "notification" ?
+                    <p className="without-notifications">
+                        Nenhuma notificação de solicitação de auxílio.
+                    </p>    
                     :
-                    <div>
+                    <form onSubmit={handleInsertUpdate}>
                         <Schedule 
-                            from=""
-                            to=""
-                            weekday=""
-                        /> 
-                        <Schedule 
-                            from=""
-                            to=""
-                            weekday=""
+                            scheduleItems={scheduleItems}
+                            alterSchedule={setScheduleItemValue}
                         />
                     
                         <div className="agenda-buttons">
                             <div className="new-schedule">
-                                <button type="button" >Incluir Novo</button>
+                                <button type="button" onClick={addNewScheduleItem}>Incluir Novo</button>
                             </div>
                             <div className="save-schedules">
-                                <button type="button" >Salvar Alterações</button>
+                                <button type="submit" >Salvar Alterações</button>
                             </div>
                         </div> 
-                    </div> 
+                    </form> 
                 }
             </main>
         </div>
